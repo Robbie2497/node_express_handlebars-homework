6b4fcfd8969ddc162da7815c90bdc0885258b03a
Robert McGrew <robbie2497@gmail.com>
2020-08-13 22:34:00 -0600

    uploading homework files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steak Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Colossal Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monster Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Elk Burger', FALSE);
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var expresshbrs = require("express-handlebars");
+
+app.engine("handlebars", expresshbrs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
+
+app.listen(PORT, function() {
+  console.log("Listening on port: ", PORT);
+});
