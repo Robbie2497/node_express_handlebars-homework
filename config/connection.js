4c3dee29d35d00238d7579e8fb6cbf0a1e9853bf
Robert McGrew <robbie2497@gmail.com>
2020-08-18 22:03:09 -0600

    creating JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Softwaredev269",
-  database: "burgers_db"
-});
+var connection;
 
 
-connection.connect(function(err) {
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Softwaredev269",
+    database: "burgers_db"
+  });
+}
+
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
